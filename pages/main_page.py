78e57fc961a6e86f573cd6d2b95557bad84b189b
unknown <niranjanaa.arun@gmail.com>
2024-11-08 15:57:10 -0600

    changes to task 1 of internship

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,7 +10,7 @@ class MainPage(Page):
     OFFPLAN= (By.CSS_SELECTOR, 'address.menu-twobutton')
     OFFPLAN_TITLE= (By.CSS_SELECTOR, "[class*='page-title off_plan']")
     NEXT_PAGE = (By.CSS_SELECTOR, "a[wized='nextPageProperties']")
-    PREVIOUS_PAGE = (By.CSS_SELECTOR, "a[wized='properties-pagination']")
+    PREVIOUS_PAGE = (By.CSS_SELECTOR, "div[wized='previousPageProperties']")
     CURRENT_PAGE = (By.CSS_SELECTOR, "div[wized='currentPageProperties']")
     TOTAL_PAGES = (By.CSS_SELECTOR, "div[wized='totalPageProperties']")
 
@@ -25,28 +25,25 @@ class MainPage(Page):
 
     def next_page(self):
         while True:
-            try:
-                current_page_number = self.find_element(*self.CURRENT_PAGE)
-                total_page_number = self.find_element(*self.TOTAL_PAGES)
-                if current_page_number == total_page_number:
+                current_page_element = self.find_element(*self.CURRENT_PAGE)
+                total_page_element = self.find_element(*self.TOTAL_PAGES)
+
+                current_page_number = int(current_page_element.text)
+                total_page_number = int(total_page_element.text)
+
+                if current_page_number >= total_page_number:
                     print("Reached the last page.")
                     break
-                self.find_element(*self.NEXT_PAGE).click()
+                self.wait_to_be_clickable_click(*self.NEXT_PAGE)
                 sleep(2)
-            except(NoSuchElementException, StaleElementReferenceException):
-                print("Next button not found or stale. Assuming last page reached.")
-                break
+
 
     def prev_page(self):
         while True:
-            try:
-                current_page_number = self.find_element(*self.CURRENT_PAGE)
+                current_page_number = int(self.find_element(*self.CURRENT_PAGE).text)
                 if current_page_number == 1:
                     print("Reached first page")
                     break
-            except(NoSuchElementException, StaleElementReferenceException):
-                print("Next button not found")
-                break
-
-
+                self.find_element(*self.PREVIOUS_PAGE).click()
+                sleep(2)
 
